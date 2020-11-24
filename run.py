f7cf327d9e2610b5cd43e280bc9eb43a5bd0e06c
Keigh Rim <keigh.rim@gmail.com>
2020-11-23 23:35:34 -0500

    trying to add filters to ingredients

--- run.py
+++ run.py
@@ -1,22 +1,30 @@
 import json
 import sys
 from datetime import datetime
+import requests
 
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 
 es_index_name = f"allrecipes-{datetime.now().strftime('%y-%m-%d-%H%M')}"
 es_index = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-es_index.indices.create(es_index_name, body=json.load(open('mappings.json')))
+
+measures = "teaspoon teaspoons tablespoon tablespoons ounce cup oz"
+with open('mappings.json') as mapping_f:
+    config = json.load(mapping_f)
+    config["settings"]["analysis"]["analyzer"]["ingredient_analyzer"]["filter"]["unit_filter"]["stopwords"]\
+        = measures.split()
+es_index.indices.create(es_index_name, body=config)
 idx_field_name = 'idx'
 
-
+ingredient_stopwords = ""
 def get_recipes(json_fname):
     with open(json_fname) as in_f:
         for idx, (url, recipe) in enumerate(json.load(in_f).items()):
             recipe[idx_field_name] = idx
             recipe['url'] = url
             recipe['brat_url'] = f"http://tarski.cs-i.brandeis.edu:8182/#/{idx}"
+            recipe['ingredients'] = [ing.replace("* ", "") for ing in recipe['ingredients'] if len(ing.strip()) > 0]
             yield recipe
 
 
@@ -37,3 +45,4 @@ def load(es_index, elements):
 
 if __name__ == '__main__':
     load(es_index, get_recipes(sys.argv[1]))
+
